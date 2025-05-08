ac3ee2d906ded129669c90e4af1b345fd3ab8b9a
Onkabetse2002 <onkabetsenkabi3@gmail.com>
2025-05-08 14:23:43 +0200

    submit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            
+            string userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out guess))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+                continue; // Skips the rest of the loop and asks again
+            }
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            } 
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower"); // Added missing semicolon
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
