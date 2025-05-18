d7a3497dd28d2d2c1004c72fb4b36600b6329e03
Onkabetse2002 <onkabetsenkabi3@gmail.com>
2025-05-18 16:10:40 +0200

    submit

---
+++ week02/journal.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    public List<Entry> Entries { get; private set; }
+
+    public Journal()
+    {
+        Entries = new List<Entry>();
+    }
+
+    public void AddEntry(Entry entry)
+    {
+        Entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in Entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in Entries)
+            {
+                writer.WriteLine($"{entry.Date}|{entry.Prompt}|{entry.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        Entries.Clear();
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split('|');
+                if (parts.Length == 3)
+                {
+                    Entries.Add(new Entry(parts[1], parts[2]) { Date = parts[0] });
+                }
+            }
+        }
+    }
+}
