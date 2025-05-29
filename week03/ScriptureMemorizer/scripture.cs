72bb9c9147d514413356ff83de913708c37e7d3a
Onkabetse2002 <onkabetsenkabi3@gmail.com>
2025-05-29 21:43:00 +0200

    submit

---
+++ week03/ScriptureMemorizer/scripture.cs
@@ -0,0 +1,23 @@
+// Scripture.cs
+public class Scripture
+{
+    // Attributes and methods
+}
+
+// Word.cs
+public class Word
+{
+    // Attributes and methods
+}
+
+// Reference.cs
+public class Reference
+{
+    // Attributes and methods
+}
+
+// ScriptureMemorizer.cs
+public class ScriptureMemorizer
+{
+    // Attributes and methods
+}
